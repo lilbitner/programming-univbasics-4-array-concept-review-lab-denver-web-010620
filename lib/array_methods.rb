7708153265037477e669e09e6718a0d5b516d0de
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-03 20:32:29 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,17 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  while array[counter] do
+  puts array.index[(array[counter])]
+  counter +=1 
 end
 
 def find_max_value(array)
-  # Add your solution here
+ while array[counter] do 
+   puts array.max 
+   counter +=1 
 end
 
 def find_min_value(array)
-  # Add your solution here
+  while array[counter] do 
+    puts array.min 
+    counter +=1 
 end
