da9e3bd35fec3e098d23a518612985661033815c
Lillian Bitner <lillian.bitner@gmail.com>
2020-01-08 21:03:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,32 @@
 def find_element_index(array, value_to_find)
-  while array[counter] do
-  puts array.index[(array[counter])]
-  counter +=1 
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+  end
+  nil
 end
 
 def find_max_value(array)
- while array[counter] do 
-   puts array.max 
-   counter +=1 
+  max = 0
+    array.length.times do |count|
+      if array[count] > max
+        max = array[count]
+      end
+    end
+  max
 end
 
 def find_min_value(array)
-  while array[counter] do 
-    puts array.min 
-    counter +=1 
+  min = 0
+    array.length.times do |count|
+      if count == 0
+        min = array[count]
+      else
+        if array[count] < min
+          min = array[count]
+        end
+      end
+    end
+  min
 end
